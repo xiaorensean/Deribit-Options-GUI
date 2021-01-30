3dc88217525dfe020d08fe3f5ad15ab6d82953c9
xiaorensean <50184308+xiaorensean@users.noreply.github.com>
2021-01-29 23:17:40 -0500

    Update influxdb_client_host_test.py

--- influxdb_client/influxdb_client_host_test.py
+++ influxdb_client/influxdb_client_host_test.py
@@ -19,8 +19,8 @@ database = 'md_rates'
 class InfluxClientHostTest:
     
     def __init__(self):        
-        user_name = 'xren'
-        passwords = '5X%UZ^Xa.bH@9Ze6'         
+        user_name = ''
+        passwords = ''         
         self.client = InfluxDBClient(host=HOST_test, port=8086, username=user_name, password=passwords)
         self.client.switch_database(database)
         #self.dfclient = DataFrameClient(host=HOST,port=8086, username=user_name, password=passwords)
